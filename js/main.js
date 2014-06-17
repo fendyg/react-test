0d4433aee390d367981c90a33ace657dc7f6b21d
fendy.g.santoso <fendy.g.santoso@accenture.com>
2014-06-17 08:45:55 -0700

    first commit, react between component

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/** @jsx React.DOM */
+
+var SearchBox = React.createClass({
+    getInitialState: function(){
+        return {
+            value: 'ProJSReact'
+        }
+    },
+
+    onInputChange: function(e){
+        this.setState({value: e.target.value});
+        this.props.onChange(e);
+    },
+
+    render: function() {
+        return new React.DOM.div({},
+            new React.DOM.p({}, 'Search box'),
+            new React.DOM.input({
+                placeholder: this.props.defaultText,
+                value: this.state.value,
+                onChange: this.onInputChange
+            })
+        );
+    }
+});
+
+var globalValue = '';
+
+var render = function(){
+    React.renderComponent(
+        new React.DOM.div({},
+            SearchBox({
+                defaultText: 'my text',
+                onChange: function(e){
+                    globalValue = e.target.value;
+                    render();
+                }
+            }),
+            new React.DOM.p({}, globalValue)
+        ),
+        document.body
+    );
+};
+
+render();
